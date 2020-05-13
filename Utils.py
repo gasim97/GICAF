d06b07643ccb3a591a5934ce2dbe7d88468ba6bd
gasim97 <gasim97@gmail.com>
2020-05-13 12:05:17 +0100

    Updated exceptions

--- Utils.py
+++ Utils.py
@@ -15,8 +15,7 @@ def saved_model_to_tflite(saved_model_path, model_name, bit_width=8):
     if (bit_width == 16):
         converter.target_spec.supported_types = [tf.float16]
     elif (bit_width != 8 and bit_width != 32):
-        print("Got a bit_width of " + str(bit_width) + ". Expected 8, 16 or 32.")
-        raise ValueError
+        raise ValueError('Expected a bit width 8, 16 or 32. Got a bit width of {}.'.format(bit_width))
     tflite_model = converter.convert()
     tflite_models_dir = Path("tmp/tflite_models/" + model_name + "/")
     tflite_models_dir.mkdir(exist_ok=True, parents=True)
