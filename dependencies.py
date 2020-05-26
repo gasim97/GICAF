386384fb0589f787318cd8655fcd35418f17c1d6
gasim97 <gasim97@gmail.com>
2020-05-26 14:38:21 +0100

    Utils edit

--- dependencies.py
+++ dependencies.py
@@ -4,8 +4,11 @@ from sys import executable
 def install_dependencies():
     subprocess.check_call([executable, "-m", "pip", "install", "git+https://github.com/Xilinx/brevitas.git"])
     subprocess.check_call([executable, "-m", "pip", "install", "git+https://github.com/alvarorobledo/foolbox.git"])
+    subprocess.check_call([executable, "-m", "pip", "install", "opencv-python"])
     subprocess.check_call([executable, "-m", "pip", "install", "tensorflow"])
     subprocess.check_call([executable, "-m", "pip", "install", "tensorflow_hub>=0.6.0"])
+    subprocess.check_call([executable, "-m", "pip", "install", "torch", "torchvision"])
     subprocess.check_call([executable, "-m", "pip", "install", "PyDrive"])
     subprocess.check_call([executable, "-m", "pip", "install", "httplib2==0.15.0"])
     subprocess.check_call([executable, "-m", "pip", "install", "google-api-python-client==1.7.11"])
+    subprocess.check_call([executable, "-m", "pip", "install", "google-colab"])
