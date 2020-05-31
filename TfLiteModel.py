f9486f09a33fc5c1be4bbc32b4f202cefcfe9d6f
gasim97 <gasim97@gmail.com>
2020-05-31 12:55:15 +0100

    EfficientNet-B0

--- TfLiteModel.py
+++ TfLiteModel.py
@@ -11,7 +11,24 @@ class ResNet50(TfLiteModel):
                                         metadata={'height': 224, 
                                                     'width': 224, 
                                                     'channels': 3,
-                                                    'bounds': (0, 255), 
+                                                    'bounds': (0, 1), 
+                                                    'bgr': False,
+                                                    'classes': 1000, 
+                                                    'weight_bits': weight_bits,
+                                                    'activation_bits': activation_bits})
+
+
+class EfficientNetB0(TfLiteModel):
+
+    # initialize
+    def __init__(self, bit_width=32): 
+        # sets up local ResNet50 model, to use for local testing
+        interpreter, weight_bits, activation_bits = utils.tfhub_to_tflite_converter("https://tfhub.dev/tensorflow/efficientnet/b0/classification/1", "efficientnet_b0", bit_width=bit_width)
+        super(EfficientNetB0, self).__init__(interpreter=interpreter, 
+                                        metadata={'height': 224, 
+                                                    'width': 224, 
+                                                    'channels': 3,
+                                                    'bounds': (0, 1), 
                                                     'bgr': False,
                                                     'classes': 1000, 
                                                     'weight_bits': weight_bits,
